0d6a07f27f8d5eede1127ad7975075410b3db5e9
Pojzo <pojzinko8@gmail.com>
2021-12-14 09:14:03 +0100

    First part

---
+++ day14/main.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+data = []
+with open("input", "r") as file:
+    for line in file:
+        if not line == '\n':
+            data.append(line[:-1])
+
+template = data[0]
+instructions = []
+dic = dict()
+for d in data[1:]:
+    instructions.append(d.split(' -> '))
+    dic[instructions[-1][0]] = instructions[-1][1]
+     
+
+steps = 10
+for step in range(steps):
+    index = 0
+    while index < len(template) - 1:
+        cur = template[index : index + 2]
+        if cur in dic:
+            template = template[:index + 1] + dic[cur] + template[index + 1:]
+
+        index += 2 
+
+counter = Counter(template)
+values = sorted(counter.values())
+print(values[-1] - values[0])
