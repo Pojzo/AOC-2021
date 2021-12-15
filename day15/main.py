7f58781e88c8ed496f7a333cd176a976d47c9e5b
Pojzo <pojzinko8@gmail.com>
2021-12-15 13:21:06 +0100

    First part

---
+++ day15/main.py
@@ -0,0 +1,40 @@
+import numpy as np
+import sys
+
+# this guy helped me a lot
+# https://www.youtube.com/watch?v=lBRtnuxg-gU
+
+X = 100
+Y = 100
+
+# X = 10
+# Y = 10
+
+matrix = []
+with open("input", "r") as file:
+    for line in file:
+        matrix.append(list(map(int, line[:-1])))
+
+def min_cost(matrix):
+    dist = np.zeros((X, Y))
+    sm = -matrix[0][0]
+    for i in range(X):
+        dist[i][0] = sm + matrix[i][0]
+        sm = dist[i][0]
+
+    sm = -matrix[0][0]
+    for j in range(Y):
+        dist[0][j] = sm + matrix[0][j]
+        sm = dist[0][j]
+
+    dist[0][0] = 0
+    for j in range(1, Y):
+        for i in range(1, X):
+            dist[i][j] = matrix[i][j] + min(dist[i - 1][j], dist[i][j - 1])
+        
+    return dist
+
+dist_matrix = min_cost(matrix)
+min_dist = int(dist_matrix[-1][-1])
+
+print(min_dist)
